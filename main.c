9caf9481ee579ca28927e3320139acb2914fabe6
Dmitry Mingazov <dmitry.mingazov@studenti.unicam.it>
2022-12-19 00:50:51 +0100

    Add circle rotation to generate sphere

--- main.c
+++ main.c
@@ -4,19 +4,19 @@
 
 #define SCREEN_WIDTH 100
 #define SCREEN_HEIGHT 100
-// distance between user and screen
-#define K1 2
 // distance between screen and object
-#define K2 5
-#define PI 3.14f
-#define RADIUS 5
+#define K2 10
+#define RADIUS 2
+// distance between user and screen
+#define K1 SCREEN_WIDTH*K2*3/(8*RADIUS*2)
 
 #define PHI_SPACING 0.05f
+#define THETA_SPACING 0.1f
 
 typedef struct s_point_3D {
-    float x;
-    float y;
-    float z;
+    double x;
+    double y;
+    double z;
 } point_3D;
 
 typedef struct s_point_2D {
@@ -26,16 +26,16 @@ typedef struct s_point_2D {
 
 void print_screen(char** screen, int w, int h);
 point_2D project_3D_to_2D(point_3D p);
-int project_point(float coord, float z);
-void draw_circle(point_3D center, float radius, char** screen, int w, int h);
+int project_point(double coord, double z);
+void draw_circle(point_3D center, double radius, char** screen, int w, int h);
 
 int main(void) {
     int z = 5;
     char** screen = (char**)malloc(sizeof(char*)*SCREEN_WIDTH);
-    float** zbuffer = (float**)malloc(sizeof(float*)*SCREEN_WIDTH);
+    double** zbuffer = (double**)malloc(sizeof(double*)*SCREEN_WIDTH);
     for(int i=0; i<SCREEN_WIDTH; i++) {
         screen[i] = (char*)malloc(sizeof(char*)*SCREEN_HEIGHT);
-        zbuffer[i] = (float*)malloc(sizeof(float*)*SCREEN_HEIGHT);
+        zbuffer[i] = (double*)malloc(sizeof(double*)*SCREEN_HEIGHT);
         for(int j=0; j<SCREEN_HEIGHT; j++) {
             screen[i][j] = ' ';
             zbuffer[i][j] = 0;
@@ -45,30 +45,50 @@ int main(void) {
     circle_center.x = 0;
     circle_center.y = 0;
     circle_center.z = 0;
-    draw_circle(circle_center, 40, screen, SCREEN_WIDTH, SCREEN_HEIGHT);
+    draw_circle(circle_center, RADIUS, screen, SCREEN_WIDTH, SCREEN_HEIGHT);
     print_screen(screen, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
-void draw_circle(point_3D center, float radius, char** screen, int w, int h) {
+void draw_circle(point_3D center, double radius, char** screen, int w, int h) {
 
-    for(float phi=0; phi < 2*PI; phi += PHI_SPACING) {
-        float cosphi = cosf(phi);
-        float sinphi = sinf(phi);
+    // loop to rotate the circle drawn
+    for(double theta=0; theta < 2*M_PI; theta += THETA_SPACING) {
+        // this loop will draw a 2d circle and apply the rotation dictated by 
+        // the outer loop
+        double costheta = cos(theta);
+        double sintheta = sin(theta);
+        for(double phi=0; phi < 2*M_PI; phi += PHI_SPACING) {
+            double cosphi = cos(phi);
+            double sinphi = sin(phi);
 
-        point_3D curr_point;
-        curr_point.x = center.x + cosphi * radius;
-        curr_point.y = center.y + sinphi * radius;
-        curr_point.z = center.z + K2;
+            // circle perpendicular to viewer, no rotations applied
+            double x = center.x + cosphi * radius;
+            double y = center.y + sinphi * radius;
+            double z = center.z;
 
-        point_2D pp = project_3D_to_2D(curr_point);
-        printf("x: %02d - y: %02d  --- phi: %f\n", pp.x, pp.y, phi);
+            // rotation matrix on y-axis
+            // [ costheta   0   sintheta ]
+            // [    0       1       0    ]
+            // [ -sintheta  0   costheta ]
+            //
+            // applying rotation around y-axis
+            point_3D curr_point;
+            curr_point.x = costheta*x + sintheta*z;
+            curr_point.y = y;
+            curr_point.z = (-sintheta)*x + costheta*z;
+            printf("phi: %.2f - (%.2f, %.2f, %.2f)\n", phi, curr_point.x, curr_point.y, curr_point.z);
 
-        pp.x += w/2;
-        pp.y += h/2;
-        if (pp.x < w && pp.y < h) {
-            screen[pp.x][pp.y] = '@';
-        } else {
-            printf("Out of bound (%d, %d)", pp.x, pp.y);
+
+            point_2D pp = project_3D_to_2D(curr_point);
+            // printf("x: %02d - y: %02d  --- phi: %f\n", pp.x, pp.y, phi);
+
+            pp.x += w/2;
+            pp.y = h/2 - pp.y;
+            if (pp.x < w && pp.y < h && pp.x >= 0 && pp.y >= 0) {
+                screen[pp.y][pp.x] = '@';
+            } else {
+                printf("Out of bound (%d, %d)\n", pp.x, pp.y);
+            }
         }
     }
 }
@@ -82,16 +102,16 @@ point_2D project_3D_to_2D(point_3D p) {
     return res;
 }
 
-int project_point(float coord, float z) {
-    printf("%d * %.2f / %.2f\n", K1, coord, z);
-    return (int) ((K1*coord)/z);
+int project_point(double coord, double z) {
+    // printf("%d * %.2f / %.2f\n", K1, coord, z);
+    return (int) ((K1*coord)/(z+K2));
 }
 
 void print_screen(char** screen, int w, int h) {
     system("clear");
-    for(int i=0; i<w; i++) {
-        for(int j=0; j<h; j++) {
-            printf(" %c", screen[i][j]);
+    for(int y=0; y<w; y++) {
+        for(int x=0; x<h; x++) {
+            printf(" %c", screen[y][x]);
         }
         printf("\n");
     }
